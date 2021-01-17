4e27924d9c4b08136b63b35095ccf2d64331e9f5
Manilator <manilator777@gmail.com>
2021-01-17 18:32:58 +0000

    base files

---
+++ restapi/dbConfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+    user          : "grupo10",
+    password      : "grupo10",
+    connectString : "oracledb:1521/aebd_tables.localdomain"
+};
---
+++ wrapper/dbConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+    internal: {
+        user          : "system",
+        password      : "Oradoc_db1",
+        connectString : "oracledb:1521/orclpdb1.localdomain"
+    },
+    external: {
+        user          : "grupo10",
+        password      : "grupo10",
+        connectString : "oracledb:1521/aebd_tables.localdomain"
+    }
+};
