4cdb44f72afc9943ffa58d7d774b06d42554f69b
Manilator <manilator777@gmail.com>
2021-01-23 20:48:00 +0000

    RestAPI done

--- restapi/server.js
+++ restapi/server.js
@@ -1,5 +1,3 @@
-// Banana Farmer Web Service Demo
-
 'use strict';
 
 var express = require('express');
@@ -151,6 +149,32 @@ app.get('/users',cors(), function (req, res) {
   });
 });
 
+// HTTP Method: GET
+// URI        : /database
+// Get last database record
+app.get('/database/:REQUESTS',cors(), function (req, res) {
+  doGetConnection(res, function(err, connection) {
+    if (err)
+      return;
+    connection.execute(
+      `select record_date "Date", name "Name", os "OS", version "Version" from database ORDER BY record_date DESC FETCH FIRST :r ROWS ONLY`,
+      { r: req.params.REQUESTS },
+      function (err, result) {
+        if (err) {
+          res.set('Content-Type', 'application/json');
+          res.status(500).send(JSON.stringify({
+            status: 500,
+            message: "Error getting the database last records",
+            detailed_message: err.message
+          }));
+        } else {
+          res.contentType('application/json').status(200);
+          res.send(JSON.stringify(result.rows));
+        }
+        doRelease(connection, "GET /database" + req.params.REQUESTS);
+      });
+  });
+});
 
 // HTTP Method: GET
 // URI        : /performance
@@ -160,7 +184,7 @@ app.get('/performance/:REQUESTS',cors(), function (req, res) {
     if (err)
       return;
     connection.execute(
-      `select cpus "Number of CPUs", cpu_usage "CPU Usage", threads "Number of Threads", time_consumed "Time Consumed" from performance where ROWNUM <= :r ORDER BY ID DESC`,
+      `select cpus "Number of CPUs", cpu_usage "CPU Usage", threads "Number of Threads", time_consumed "Time Consumed" from performance ORDER BY id DESC FETCH FIRST :r ROWS ONLY`,
       { r: req.params.REQUESTS },
       function (err, result) {
         if (err) {
@@ -179,154 +203,29 @@ app.get('/performance/:REQUESTS',cors(), function (req, res) {
   });
 });
 
-// HTTP method: GET
-// URI        : /bananas/FARMER
-// Get the banana shipment for FARMER
-app.get('/bananas/:FARMER', function (req, res) {
+// HTTP Method: GET
+// URI        : /activiy
+// Get last activiy record
+app.get('/activity/:REQUESTS',cors(), function (req, res) {
   doGetConnection(res, function(err, connection) {
     if (err)
       return;
     connection.execute(
-      "SELECT b.shipment FROM bananas b WHERE b.shipment.farmer = :f",
-      { f: req.params.FARMER },
+      `select sessions "Number of sessions" from activity ORDER BY id DESC FETCH FIRST :r ROWS ONLY`,
+      { r: req.params.REQUESTS },
       function (err, result) {
         if (err) {
           res.set('Content-Type', 'application/json');
           res.status(500).send(JSON.stringify({
             status: 500,
-            message: "Error getting the farmer's profile",
-            detailed_message: err.message
-          }));
-        } else if (result.rows.length < 1) {
-          res.set('Content-Type', 'application/json');
-          res.status(404).send(JSON.stringify({
-            status: 404,
-            message: "Farmer doesn't exist",
-            detailed_message: ""
-          }));
-        } else {
-          res.contentType('application/json');
-          res.status(200).send(JSON.stringify(result.rows));
-        }
-        doRelease(connection, "GET /bananas/" + req.params.FARMER);
-      });
-  });
-});
-
-// HTTP method: POST
-// URI        : /bananas
-// Create a new banana shipment for a farmer
-app.post('/bananas', function (req, res) {
-  if ("application/json" !== req.get('Content-Type')) {
-    res.set('Content-Type', 'application/json').status(415).send(JSON.stringify({
-      status: 415,
-      message: "Wrong content-type. Only application/json is supported",
-      detailed_message: null
-    }));
-    return;
-  }
-  doGetConnection(res, function(err, connection) {
-    if (err)
-      return;
-    connection.execute(
-      "INSERT INTO bananas VALUES (:s)",
-      { s: JSON.stringify(req.body) },
-      { autoCommit: true },
-      function (err) {
-        if (err) {
-          res.set('Content-Type', 'application/json');
-          res.status(400).send(JSON.stringify({
-            status: 400,
-            message: "Input Error",
+            message: "Error getting the activiy last records",
             detailed_message: err.message
           }));
         } else {
-          // Successfully created the resource
-          res.status(201).set('Location', '/bananas/' + req.body.FARMER).end();
+          res.contentType('application/json').status(200);
+          res.send(JSON.stringify(result.rows));
         }
-        doRelease(connection, "POST /bananas");
-      });
-  });
-});
-
-// HTTP method: PUT
-// URI        : /bananas/FARMER
-// Update the banana shipment for FARMER
-app.put('/bananas/:FARMER', function (req, res) {
-  if ("application/json" !== req.get('Content-Type')) {
-    res.set('Content-Type', 'application/json').status(415).send(JSON.stringify({
-      status: 415,
-      message: "Wrong content-type. Only application/json is supported",
-      detailed_message: null
-    }));
-    return;
-  }
-  doGetConnection(res, function(err, connection) {
-    if (err)
-      return;
-    connection.execute(
-      "UPDATE bananas b SET shipment = :s WHERE b.shipment.farmer = :f",
-      {
-        s: JSON.stringify(req.body),
-        f: req.params.FARMER
-      },
-      { autoCommit: true },
-      function (err, result) {
-        if (err) {
-          res.set('Content-Type', 'application/json');
-          res.status(400).send(JSON.stringify({
-            status: 400,
-            message: "Input Error",
-            detailed_message: err.message
-          }));
-        } else if (result.rowsAffected === 0) {
-          res.set('Content-Type', 'application/json');
-          res.status(400).send(JSON.stringify({
-            status: 400,
-            message: "Farmer doesn't exist",
-            detailed_message: ""
-          }));
-        } else {
-          // Resource successfully updated. Sending an empty response body.
-          res.status(204).end();
-        }
-        doRelease(connection,"PUT /bananas/" + req.params.FARMER);
-      });
-  });
-});
-
-// HTTP method: DELETE
-// URI        : /bananas/FARMER
-// Delete banana shipments for FARMER
-app.delete('/bananas/:FARMER', function (req, res) {
-  doGetConnection(res, function(err, connection) {
-    if (err)
-      return;
-    connection.execute(
-      "DELETE FROM bananas b WHERE b.shipment.farmer = :f",
-      { f: req.params.FARMER },
-      { autoCommit: true },
-      function (err, result) {
-        if (err) {
-          res.set('Content-Type', 'application/json');
-          res.status(400).send(JSON.stringify({
-            status: 400,
-            message: "Input Error",
-            detailed_message: err.message
-          }));
-        } else if (result.rowsAffected === 0) {
-          res.set('Content-Type', 'application/json');
-          res.status(400).send(JSON.stringify({
-            status: 400,
-            message: "Farmer doesn't exist",
-            detailed_message: ""
-          }));
-        } else {
-          // Resource successfully deleted. Sending an empty response body.
-          res.status(204).end();
-        }
-
-        doRelease(connection, "DELETE /bananas/" + req.params.FARMER);
+        doRelease(connection, "GET /activiy" + req.params.REQUESTS);
       });
   });
 });
